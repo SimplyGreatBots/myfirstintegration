86cbf8f578f9fecb5c37f2415d35302a1233341c
SimplyGreatBots <157548892+SimplyGreatBots@users.noreply.github.com>
2024-02-08 14:11:16 -0500

    Update integration.definition.ts

--- integration.definition.ts
+++ integration.definition.ts
@@ -3,7 +3,7 @@ import { name } from './package.json'
 
 export default new IntegrationDefinition({
   name,
-  description: 'This is my first integration!',
+  description: 'This is my first integration! Wahoo!',
   version: '0.2.0',
   channels: {
     channel: {
